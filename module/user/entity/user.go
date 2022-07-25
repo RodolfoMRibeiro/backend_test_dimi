7782961db1fa25dcb5dc091d981d6d14ab9239e7
Rodolfo Marques Ribeiro <rodolfomarqribeiro@gmail.com>
2022-07-25 10:04:30 -0300

    fix(db, module/user/entity): create json tag in user struct and fix migrate

--- module/user/entity/user.go
+++ module/user/entity/user.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	CpfCnpj    string           `json:"cpf_cnpj" gorm:"type:varchar(14);primaryKey;autoIncrement:false"`
-	Email      string           `gorm:"type:varchar(25)"`
+	Email      string           `json:"email" gorm:"type:varchar(25)"`
 	Account    []entity.Account `json:"account" gorm:"foreignKey:CpfCnpj"`
 	IdCategory int              `json:"id_category"`
 	FullName   string           `json:"full_name" gorm:"type:varchar(30)"`
