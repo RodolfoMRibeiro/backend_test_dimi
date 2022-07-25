cd78e0bab494822c36a3bd3e38350cf0f226fbd0
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2022-07-24 21:27:09 -0300

    feat: for now it has some bugs in controllers('cause it's not finished yet), but can be approved for sure WITHOUT FEARgit add .!

--- db/entity/environmentVariables.go
+++ db/entity/environmentVariables.go
@@ -5,12 +5,12 @@ import (
 )
 
 type EnvironmentVariables struct {
-	Database string `env:"DATABASE"`
-	Port     string `env:"PORT" envDefault:"3306"`
-	Host     string `env:"HOST"`
-	User     string `env:"USER"`
-	Password string `env:"PASSWORD"`
-	Configs  string `env:"CONFIGS"`
+	Database string `env:"db_DATABASE"`
+	Port     string `env:"db_PORT" envDefault:"3306"`
+	Host     string `env:"db_HOST"`
+	User     string `env:"db_USER"`
+	Password string `env:"db_PASSWORD"`
+	Configs  string `env:"db_CONFIGS"`
 }
 
 func (env *EnvironmentVariables) FeedStruct() {
