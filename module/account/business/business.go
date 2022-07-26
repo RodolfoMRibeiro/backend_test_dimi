c014e6a13a0388c356c28dd6f390a2bc26ccc597
Rodolfo Marques Ribeiro <rodolfomarqribeiro@gmail.com>
2022-07-26 15:17:25 -0300

    style: remove all repository and buisiness folders

--- module/account/business/business.go
+++
@@ -1,5 +0,0 @@
-package business
-
-// can't exist two or more e-mails or CPF/CNPJ equal registed in database
-// shopkeeper can only revece money in transactions
-// An account can't transact money to itself
