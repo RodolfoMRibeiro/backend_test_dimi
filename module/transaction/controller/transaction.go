4f97c9d98e4ffda478d06c074ab0cb7964a86f7f
R-o-d-r-i-g-o <rodrigo.ribeiro@q2pay.com.br>
2022-07-26 11:01:39 -0300

    fix: some funcs now are corrected but not all of them

--- module/transaction/controller/transaction.go
+++ module/transaction/controller/transaction.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"net/http"
 	"transaction/db"
+	controller_account "transaction/module/account/controller"
 	entity_transaction "transaction/module/transaction/entity"
 
 	"github.com/gin-gonic/gin"
@@ -19,12 +20,16 @@ func CreateTransaction(c *gin.Context) {
 		return
 	}
 
-	if ok := db.DB.Where("tb_users.id_category = ? and tb_accounts.id = tb.transactions.id_payee", 2).Error; ok != nil {
+	// db.DB.Where().Create(&NewTransaction)
 
-		db.DB.Table("tb_transactions").Create(&NewTransaction)
-	}
+	db.DB.Joins("FROM tb_transactions A RIGHT JOIN tb_accounts ON A.id_payer = ?").Where(NewTransaction.IdPayer).Find(&controller_account.NewAccount)
+	// .Joins("RIGHT JOIN tb_accounts.cpf_cnpj ON tb_users.cpf_cnpj = ?", NewTransaction.IdPayer).Find(&NewTransaction)
+	// db.DB.Table("tb_accounts").Where("id = ?", NewTransaction.).find(&status).Err
+	// fmt.Println(controller_account.NewAccount.CpfCnpj)
 
-	c.JSON(http.StatusOK, gin.H{"New user registred": NewTransaction})
+	// db.DB.Table("tb_transactions")
+
+	c.JSON(http.StatusOK, gin.H{"New user registred": controller_account.NewAccount})
 }
 
 func FindTransaction(c *gin.Context) {
