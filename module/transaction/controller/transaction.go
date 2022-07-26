de6d799b66860a939046214960d4a11fa8f71c95
Rodolfo Marques Ribeiro <rodolfomarqribeiro@gmail.com>
2022-07-26 17:41:13 -0300

    style: modify transaction

--- module/transaction/controller/transaction.go
+++ module/transaction/controller/transaction.go
@@ -8,25 +8,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var NewTransaction *entity_transaction.Transaction
-
-var NewTransactions *[]entity_transaction.Transaction
-
 func CreateTransaction(c *gin.Context) {
-
+	var NewTransaction *entity_transaction.Transaction
 	if err := c.BindJSON(&NewTransaction); err != nil {
 		c.IndentedJSON(http.StatusNotAcceptable, "wrong data inserted") // 406
 		return
 	}
-
-	// db.DB.Joins("FROM tb_transactions A RIGHT JOIN tb_accounts ON A.id_payer = ?").Where(NewTransaction.IdPayer).Find(&controller_account.NewAccount)
-
-	// c.JSON(http.StatusOK, gin.H{"New user registred": controller_account.NewAccount})
-
+	if NewTransaction.IdPayer != NewTransaction.IdPayee {
+		NewTransaction.ValidateTransaction()
+		db.DB.Table("tb_transactions").Create(&NewTransaction)
+		c.JSON(http.StatusOK, gin.H{"New user registred": NewTransaction})
+	}
 }
-
 func FindTransaction(c *gin.Context) {
-
+	var NewTransactions *[]entity_transaction.Transaction
 	db.DB.Find(&NewTransactions)
 	c.JSON(http.StatusOK, NewTransactions)
 }
