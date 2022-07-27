110da9cf19fec1d8f79a0a123f1eba15ba0ed3e9
R-o-d-r-i-g-o <rodrigo.ribeiro@q2pay.com.br>
2022-07-27 16:36:22 -0300

    feat: already done some constants in library

--- library/consts.go
+++ library/consts.go
@@ -1,1 +1,8 @@
 package library
+
+// refer to database:
+const TB_USERS = "tb_users"
+const TB_STATUS = "tb_status"
+const TB_ACCOUNTS = "tb_accounts"
+const TB_CATEGORIES = "tb_categories"
+const TB_TRANSACTIONS = "tb_transactions"
