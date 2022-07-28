075711e94560fbd285d6fb157393c4ca20393dd4
Rodolfo Marques Ribeiro <rodolfomarqribeiro@gmail.com>
2022-07-28 18:35:36 -0300

    style: change functions order inside integrations folder

--- integrations/integrations.go
+++ integrations/integrations.go
@@ -11,10 +11,6 @@ type TransactionStatus struct {
 	Authorization bool `json:"authorization"`
 }
 
-func (b *TransactionStatus) feedTransactionStruct(jsonElement string) {
-	json.Unmarshal([]byte(jsonElement), b)
-}
-
 func (b *TransactionStatus) ConnectWithExternalAPI() {
 	response, err := http.Get(`https://run.mocky.io/v3/d02168c6-d88d-4ff2-aac6-9e9eb3425e31`)
 	util.PresentateErros(err)
@@ -23,4 +19,8 @@ func (b *TransactionStatus) ConnectWithExternalAPI() {
 	util.PresentateErros(err)
 
 	b.feedTransactionStruct(string(responseData))
+}
+
+func (b *TransactionStatus) feedTransactionStruct(jsonElement string) {
+	json.Unmarshal([]byte(jsonElement), b)
 }
